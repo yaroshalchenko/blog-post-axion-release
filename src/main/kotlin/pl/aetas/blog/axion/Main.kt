180426c72de9df0aa6c3c7905e22ec857d575182
yaroshalchenko <Oksi123V>
2020-07-05 01:23:07 +0200

    Main added

--- src/main/kotlin/pl/aetas/blog/axion/Main.kt
+++ src/main/kotlin/pl/aetas/blog/axion/Main.kt
@@ -1,6 +1,6 @@
 package pl.aetas.blog.axion
 
 fun main() {
-  println("Hello from your app from Yaroslav Test for release next version fdsfds")
+  println("Hello from your app from Yaroslav Test for release next version")
 }
 
